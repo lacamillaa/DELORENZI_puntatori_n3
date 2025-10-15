b00a295932314e469a06727dedbc0ee7da50d3ff
lacamillaa <12999356@itisrossi.vi.it>
2025-10-15 17:58:09 +0200

    create_array function

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* create_array(int n, int initial_value) {
+    int* arr = (int*)malloc(sizeof(int) * n);
+    if (arr == NULL) return NULL;
+    for (int i = 0; i < n; i++) {
+        *(arr + i) = initial_value;
+    }
+    return arr;
+}
 
 int main(void) {
     printf("Hello, World!\n");
